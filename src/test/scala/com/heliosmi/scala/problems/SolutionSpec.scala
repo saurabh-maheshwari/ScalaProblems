db7626ee716610c72890e29198365af9e9f8aaa0
Saurabh Maheshwari <maheshwari.saurabh@outlook.com>
2015-07-26 16:34:58 -0600

    Pushing 2 tests

--- src/test/scala/com/heliosmi/scala/problems/SolutionSpec.scala
+++ src/test/scala/com/heliosmi/scala/problems/SolutionSpec.scala
@@ -1,7 +1,17 @@
 package com.heliosmi.scala.problems
 
 import org.scalatest._
+import com.heliosmi.scala.problems.ScalaProblems._
+class SolutionSpec extends FlatSpec with Matchers {
 
-class SolutionSpec extends FlatSpec with Matchers{
+  it should "Find the last element of the list" in {
+    assert(lastElement(List(1, 2, 3)) === 3)
+    assert(lastElement(List('a', 'b', 'c')) === 'c')
+  }
+
+  it should "Find the last but one element of a list." in {
+	  assert(penultimateElement(List(1,2,3)) === 2)
+	  assert(penultimateElement(List('a','b','c')) == 'b')
+  }
 
 }
